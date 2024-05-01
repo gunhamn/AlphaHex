45255e6bd0a834c48a63f4414c596105bb06fe21
Emily Louise Malcolmson <emilylm@stud.ntnu.no>
2024-05-01 11:59:37 +0200

    fixed topp bug

--- code/TOPP.py
+++ code/TOPP.py
@@ -2,6 +2,8 @@ from alphahex import GameHex
 import numpy as np
 from ANET_tf import ANET_tf
 import keras
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 class TOPP:
     def __init__(self, networks, game:GameHex) -> None:
         self.score=[0]*len(networks)
@@ -10,27 +12,36 @@ class TOPP:
         pass
 
     def tournament(self, numberOfRounds):
-        for i in range(len(self.networks)):
-            for j in range(len(self.networks)):
-                if self.networks[i]!=self.networks[j]:
-                    self.play(self.networks[i], self.networks[j], i, j, numberOfRounds)
-                    self.play(self.networks[j], self.networks[i], j, i, numberOfRounds)
+        networks = self.networks
+        num_networks = len(networks)
+        
+        for i in tqdm(range(num_networks)):
+            for j in range(i + 1, num_networks):  # Iterate over remaining networks
+                self.play(networks[i], networks[j], i, j, numberOfRounds)
+                self.play(networks[j], networks[i], j, i, numberOfRounds)
         pass
 
     def play(self, player1, player2, player1Nr, player2Nr, numberOfRounds):
         for gameNr in range(numberOfRounds):
+            #print(f"Game {gameNr}, player1: {player1Nr}, player2, {player2Nr}")
             self.game.reset()
             while self.game.isFinalState()==None:
+                #self.game.printGameState()
                 if self.game.getBoardState()[0]==1:
-                    action = np.argmax(player1.forward(self.game.getBoardState()))
+                    action = np.argmax(player1.forward(self.game.getBoardState(), self.game.getMoves()))
+                    #print(f"action: {action}")
                     self.game.update(move=action)
                 else:
-                    action = np.argmax(player2.forward(self.game.getBoardState()))
+                    action = np.argmax(player2.forward(self.game.getBoardState(), self.game.getMoves()))
                     self.game.update(move=action)
-            if self.game.isFinalState==1:
+            #print(f"finalstate: {self.game.isFinalState()}")
+            if self.game.isFinalState()==1:
+                #print("player 1 won")
                 self.score[player1Nr]+=1
             else:
+                #print("player 2 won")
                 self.score[player2Nr]+=1
+        print(f"score {self.score}, player1 {player1Nr}, player2 {player2Nr}, numberofGames: {numberOfRounds}")
         pass
 
 if __name__ == "__main__":
@@ -43,11 +54,25 @@ if __name__ == "__main__":
     player2 = ANET_tf(numInput=numInput, numOutput=numOutput)
     player2.model = keras.models.load_model('code/networks/network_10.keras')
     player3 = ANET_tf(numInput=numInput, numOutput=numOutput)
-    player3.model = keras.models.load_model('code/networks/network_20.keras')
+    player3.model = keras.models.load_model('code/networks/network_15.keras')
     player4 = ANET_tf(numInput=numInput, numOutput=numOutput)
-    player4.model = keras.models.load_model('code/networks/network_50.keras')
+    player4.model = keras.models.load_model('code/networks/network_25.keras')
 
     networks=[player1, player2, player3, player4]
     topp = TOPP(networks=networks, game=game)
-    topp.tournament(10)
+    print("RUNBEGINS")
+    topp.tournament(25)
+    print(topp.score)
+    plt.plot([1,2,3,4], topp.score)
+    plt.xlabel('players')
+    plt.ylabel('wins')
+    plt.title('Histogram')
+    plt.show()
+
+    plt.bar([1,2,3,4], topp.score, color='blue')
+    plt.xlabel('players')
+    plt.ylabel('wins')
+    plt.title('Histogram')
+    plt.grid(True)  # Add grid lines for better readability
+    plt.show()
     #plot histogram with score
